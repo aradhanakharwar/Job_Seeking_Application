625ed9e8863cd8053f3a8e9f357bee95c57b06c8
Aradhana Kharwar <kharwararadhana2001@gmail.com>
2024-06-29 10:26:53 +0530

    chnages for token

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,13 +4,15 @@ export const sendToken = (user, statusCode, res, message) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
-    };
-    res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production', // use secure cookies in production
-        sameSite: 'None' // required for cookies to be sent in cross-site requests
-    });
+        sameSite: 'None'
+    };
+    // res.cookie('token', token, {
+    //     httpOnly: true,
+    //     secure: process.env.NODE_ENV === 'production', // use secure cookies in production
+    //     sameSite: 'None' // required for cookies to be sent in cross-site requests
+    // });
     res.status(statusCode).cookie("token", token, options).json({
         success: true,
         user,
